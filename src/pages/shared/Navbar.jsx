3f755ca58c039b39e71fda064ff71d12b6315351
Md Noble <mdnoble.wd@gmail.com>
2023-10-19 19:37:15 +0600

    added navbar

--- src/pages/shared/Navbar.jsx
+++ src/pages/shared/Navbar.jsx
@@ -1,9 +1,67 @@
+import { NavLink } from "react-router-dom";
+import { CgMenuLeft } from "react-icons/cg";
+import logo from "../../assets/carzane.png";
+
 const Navbar = () => {
-    return (
-        <div>
-            
+  const navLinks = (
+    <>
+      <li>
+        <NavLink
+          to="/"
+          className={({ isActive, isPending }) =>
+            isPending ? "pending" : isActive ? "text-[#ea001e] underline" : ""
+          }
+        >
+          Home
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to="/login"
+          className={({ isActive, isPending }) =>
+            isPending ? "pending" : isActive ? "text-[#ea001e] underline" : ""
+          }
+        >
+          Login
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to="/register"
+          className={({ isActive, isPending }) =>
+            isPending ? "pending" : isActive ? "text-[#ea001e] underline" : ""
+          }
+        >
+          Register
+        </NavLink>
+      </li>
+    </>
+  );
+
+  return (
+    <nav className="lg:mt-10 bg-white lg:mx-16 lg:rounded-2xl lg:drop-shadow-2xl">
+      <div className="flex justify-between items-center pl-6 lg:pr-10 py-4 lg:py-8">
+        <div className="dropdown lg:hidden">
+          <label tabIndex={0} className="text-3xl font-extrabold">
+            <CgMenuLeft></CgMenuLeft>
+          </label>
+          <ul
+            tabIndex={0}
+            className="dropdown-content mt-3 z-[1] p-4 shadow bg-base-100 rounded-box w-40
+              md:w-52 font-medium text-black"
+          >
+            {navLinks}
+          </ul>
         </div>
-    );
+        <div>
+          <img className="w-44 md:w-44 lg:w-64" src={logo} alt="" />
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="flex gap-12 font-bold text-xl text-black">{navLinks}</ul>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
